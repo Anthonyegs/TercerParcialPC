9ebacaeb732e4f66e74711386099a7bb974a11fb
anthonyegs <anthonyegs@gmail.com>
2020-11-22 17:12:16 -0600

    Metodos para crear y actualizar
    Se crearon los metodos para crear y actualizar un juego

--- juegosApp/schema.py
+++ juegosApp/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import *
+from .models import Juego
 
 
 class JuegoType(DjangoObjectType):
@@ -13,3 +13,53 @@ class Query(graphene.ObjectType):
 
     def resolve_juegos(self, info, **kwargs):
         return Juego.objects.all()
+
+
+class JuegoInput(graphene.InputObjectType):
+    id = graphene.ID()
+    nombre = graphene.String()
+    descripcion = graphene.String()
+    foto = graphene.String()
+
+
+class CreateJuego(graphene.Mutation):
+    class Arguments:
+        input = JuegoInput(required=True)
+
+    ok = graphene.Boolean()
+    juego = graphene.Field(JuegoType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        juego_instance = Juego(nombre=input.nombre,
+                               descripcion=input.descripcion, foto=input.foto)
+        juego_instance.save()
+        return CreateJuego(ok=ok, juego=juego_instance)
+
+
+class UpdateJuego(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = JuegoInput(required=True)
+
+    ok = graphene.Boolean()
+    juego = graphene.Field(JuegoType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        juego_instance = Juego.objects.get(pk=id)
+        if juego_instance:
+            ok = True
+            juego_instance.nombre = input.nombre
+            juego_instance.descripcion = input.descripcion
+            juego_instance.foto = input.foto
+            juego_instance.save()
+            return UpdateJuego(ok=ok, juego=juego_instance)
+        return UpdateJuego(ok=ok, juego=None)
+
+
+class Mutation(graphene.ObjectType):
+    create_juego = CreateJuego.Field()
+    update_juego = UpdateJuego.Field()
